0f2410431c9619c7ceecc14ab4439f8171b8fc74
kek <astanaevv@bk.ru>
2021-09-12 10:48:03 +0700

    Добавьте файлы проекта.

---
+++ lesson 19.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+class Animals
+{
+    
+public:
+    virtual void MakeSound() const = 0;
+
+};
+
+class Dog :public Animals
+{
+
+public:
+    void MakeSound() const override
+    {
+        std::cout << "Meow\n";
+    }
+
+};
+
+class Cat :public Animals
+{
+
+public: 
+    void MakeSound() const override
+    {
+        std::cout << "Woof\n";
+    }
+
+};
+
+int main()
+{
+
+    Animals* animals[2];
+    animals[0] = new Cat();
+    animals[1] = new Dog();
+
+    for (Animals* a : animals)
+        a->MakeSound();
+    
+}
